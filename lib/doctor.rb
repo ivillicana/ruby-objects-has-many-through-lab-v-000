0bb44fadccab6c72147246a52416e5ca29ca40f1
Isaac Villicana <villicana.i@gmail.com>
2018-04-18 21:12:17 +0000

    automatically backed up by learn

---
+++ lib/doctor.rb
@@ -0,0 +1,19 @@
+class Doctor
+  attr_accessor :name, :appointments
+
+  def initialize (name)
+    @name = name
+    @appointments = []
+  end
+
+  def add_appointment(appt, doctor)
+    @appointments << appt
+    appt.doctor = doctor
+  end
+
+  def patients
+    self.appointments.collect do |x|
+      x.patient 
+    end
+  end
+end
